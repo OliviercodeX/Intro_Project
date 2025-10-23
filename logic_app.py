79c6d66eeaa884e86a540e145f4cc9b890416338
Minor <greivin5324@mail.com>
2025-10-23 07:41:50 -0600

    chance of position of label and button

--- logic_app.py
+++ logic_app.py
@@ -8,11 +8,12 @@ def main_window():
     app.geometry("500x400")
     app.title("Test customtkinter")
 
-    label = ctk.CTkLabel(app, text="Si ves esto, customtkinter funciona")
-    label.pack(pady=12)
+    # Usar solo grid() — no mezclar pack() y grid()
+    label = ctk.CTkLabel(app, text="Bienvenido", font=("Times New Roman", 20))
+    label.grid(row=0, column=10, columnspan=2, pady=(12, 8), padx=12)
 
     button = ctk.CTkButton(app, text="Cerrar", command=app.destroy)
-    button.pack(pady=6)
+    button.grid(row=1, column=10, padx=12, pady=8, sticky='w')
 
     app.mainloop()
 
