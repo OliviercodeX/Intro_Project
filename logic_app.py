e6f1dd0667c3a81fd08290145a57586c8d07b6be
Minor <greivin5324@mail.com>
2025-10-22 22:20:54 -0600

    Readjustment of CustomTkinter functions.

--- logic_app.py
+++ logic_app.py
@@ -1,19 +1,21 @@
 import customtkinter as ctk
 
-# Ventana mínima de prueba
-ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("blue")
+def main_window():
+    ctk.set_appearance_mode("System")
+    ctk.set_default_color_theme("blue")
 
-app = ctk.CTk()
-app.geometry("500x400")
-app.title("Test customtkinter")
+    app = ctk.CTk()
+    app.geometry("500x400")
+    app.title("Test customtkinter")
 
-label = ctk.CTkLabel(app, text="Si ves esto, customtkinter funciona")
-label.pack(pady=12)
+    label = ctk.CTkLabel(app, text="Si ves esto, customtkinter funciona")
+    label.pack(pady=12)
 
-button = ctk.CTkButton(app, text="Cerrar", command=app.destroy)
-button.pack(pady=6)
+    button = ctk.CTkButton(app, text="Cerrar", command=app.destroy)
+    button.pack(pady=6)
+
+    app.mainloop()
 
 
 if __name__ == '__main__':
-    app.mainloop()
+    main_window()
