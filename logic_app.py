b3a934c48b222d56440eeefca9f6674ba9701a9a
Minor <greivin5324@mail.com>
2025-10-23 18:36:51 -0600

    The first function to create flights

--- logic_app.py
+++ logic_app.py
@@ -1,1 +1,13 @@
-import customtkinter
+
+
+#fuction to create flights
+flights = []
+
+#E: code(string), origin(string), destination(string), price(float), price(integer), column,column(integer), sold_count(int)
+def create_flight(code, origin, destination, price, row, column,elem, sold_count=0):
+    seat_matrix = [[elem]*row for i in range(column)]
+    
+    flights.append([code,origin,destination, price, seat_matrix,sold_count])
+    return len(flights)
+
+
